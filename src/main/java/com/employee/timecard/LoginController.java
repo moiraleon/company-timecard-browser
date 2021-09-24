e64e2ccac44fbc51471a2bfa89fd6813043275e1
Moira Leon <moira.leon2@gmail.com>
2021-09-23 20:59:24 -0600

    position of box moved

--- src/main/java/com/employee/timecard/LoginController.java
+++ src/main/java/com/employee/timecard/LoginController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
@@ -13,18 +14,18 @@ public class LoginController {
         return "login";
     }
 
-//    @GetMapping(value = "/login")
-//    public String getNewPage(Model model, @ModelAttribute LoginUser loginUser) {
-//        if (loginUser.getEmployeeID().equals("employeeID") && loginUser.getKeyCode() == loginUser.getKeyCode()) {
-//            model.addAttribute("msg", "Please Enter Employee Login Details");
-//            model.addAttribute("error", " ");
-//            return "clock-in";
-//
-//        } else {
-//            model.addAttribute("error", "Invalid Login Information. Please try again.");
-//            return "login";
-//        }
-//    }
+    @RequestMapping(value = "/login")
+    public String getNewPage(Model model, @ModelAttribute LoginUser loginUser) {
+        if (loginUser.getEmployeeID().equals("employeeID") && loginUser.getKeyCode() == loginUser.getKeyCode()) {
+            model.addAttribute("msg", "Please Enter Employee Login Details");
+            model.addAttribute("error", " ");
+            return "clock-in";
+
+        } else {
+            model.addAttribute("error", "Invalid Login Information. Please try again.");
+            return "login";
+        }
+    }
 
 
 }
